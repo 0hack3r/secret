5a984d187b584985534f1526c63d7189fb05a9be
0hack3r <155529279+0hack3r@users.noreply.github.com>
2024-11-13 08:44:24 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,25 @@
 fetch("https://my.billdu.com/accountant.settings/profile?supplierId=1448763", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "accept-language": "en-GB,en;q=0.9",
+    "cache-control": "no-cache",
+    "content-type": "application/x-www-form-urlencoded",
+    "pragma": "no-cache",
+    "priority": "u=0, i",
+    "sec-ch-ua": "\"Chromium\";v=\"130\", \"Brave\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "sec-gpc": "1",
+    "upgrade-insecure-requests": "1"
   },
-  body: "save=Save&company=testing&email=bonenes831%40gianes.com&_do=settingsForm-submit",
-  credentials: "include"
+  "referrer": "https://my.billdu.com/accountant.settings/profile?supplierId=1448763",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "save=Save&company=test&street=&city=&zip=&companyId=&taxId=&vatId=&type=I%27m+not+VAT+registered.&phone=&email=bonenes831%40gianes.com&web=testing&_do=settingsForm-submit",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
 });
